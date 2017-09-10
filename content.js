35d76a7ff7612830963ab98567e6c341e8b72b34
Aman Karunakaran <amankarunakaran@Amans-MacBook-Pro.local>
2017-09-10 18:11:33 -0400

    last one worked. Seeing if putting it on the button works

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(request.message === "clicked_browser_action") {
-			var firstHref = $("a[href^='http']").eq(0).attr("href");
-			console.log(firstHref);
+			document.getElementById('password').value = 'test';
 		}
 	}
 );
 
-document.getElementById('password').value = 'test';
