e5d8cdd50f0f66ddad7dff58795acf2a637edba1
Aman Karunakaran <amankarunakaran@Amans-MacBook-Pro.local>
2017-09-10 19:39:41 -0400

    am now able to append relevant url to text field

--- content.js
+++ content.js
@@ -2,9 +2,26 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(request.message === "clicked_browser_action") {
 			var str = document.getElementById('handle').value;
-			str = str.split("").reverse().join("");
+			//str = str.split("").reverse().join("");
+			var url = window.location.href;
+			var slash = 0;
+			var idxStart = 0;
+			var idxEnd = 0;
+			for(var i = 0; i < url.length; ++i) {
+				if(slash == 2 && idxStart == 0) {
+					idxStart = i;
+				}
+				if(url.charAt(i) == '/') {
+					if(slash == 2) {
+						idxEnd = i;
+						break;
+					}
+					++slash;
+				}
+			}
+			url = url.slice(idxStart, idxEnd);
+			str = str.concat(url);
 			document.getElementById('handle').value = str;
 		}
 	}
 );
-
